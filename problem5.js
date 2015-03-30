ef9d98cbb140985091a5f8683ba2648539cd66e2
Pyxis <ysilyutin@gmail.com>
2015-03-29 20:24:19 -0700

    Update problem5.js
    Refactoring has done

--- problem5.js
+++ problem5.js
@@ -2,6 +2,18 @@
 
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+/* Refactoring has done
+
+var i = 190;
+for (i; i; i += 190) {
+  if (i % 3 === 0 && i % 4 === 0 && i % 6 === 0 && i % 7 === 0 && i % 8 === 0 && i % 9 === 0 && i % 11 === 0 && i % 12 === 0 && i % 13 === 0 && i % 14 === 0 && i % 15 === 0 && i % 16 === 0 && i % 17 === 0 && i % 18 === 0 && i % 20 === 0) {
+    console.log(i);
+    break;
+  }
+}
+
+*/
+
 function findNumber(n) {
   var temp = [];
   var i = 0, j = 0;
